7f4fee973b11f9ed4e011d414bd94df7d50530b5
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-05-21 03:45:11 +0400

    update

--- time_series.py
+++ time_series.py
@@ -15,43 +15,44 @@ X = df.drop('target', axis=1)
 y = df.target
 preprocessor = joblib.load('./model/preprocessor.x')
 model = joblib.load('./model/model.x')
-X_test = preprocessor.transform(X)
-pred = model.predict(X_test)
+clf = Pipeline([('pre', preprocessor), ('classification', model)])
 
-# print(pred)
+pred = clf.predict(X)
+
+print(pred)
 # print(f"Accuracy { accuracy_score(y, pred)*100}")
 
-predictions = []
+# predictions = []
 
-for i in range(df.shape[0]):
-    x_test = df.values[i]
-    sleep(1)
-    pred = model.predict(x_test)
-    print(pred)
-    # if pred == 'Car':
-    #     predictions.append('Car')
-    # elif pred == 'Still':
-    #     predictions.append('Still')
-    # elif pred == 'Walking':
-    #     predictions.append('Walking')
+# for i in range(df.shape[0]):
+#     x_test = df.values[i]
+#     sleep(1)
+#     pred = model.predict(x_test)
+#     print(pred)
+# if pred == 'Car':
+#     predictions.append('Car')
+# elif pred == 'Still':
+#     predictions.append('Still')
+# elif pred == 'Walking':
+#     predictions.append('Walking')
 
-    # if len(predictions) == 3:
-    #     still_cont = 0
-    #     walk_cont = 0
-    #     car_cont = 0
+# if len(predictions) == 3:
+#     still_cont = 0
+#     walk_cont = 0
+#     car_cont = 0
 
-    #     for p in predictions:
-    #         if p == 'Still':
-    #             still_cont += 1
-    #         elif p == 'Car':
-    #             walk_cont += 1
-    #         elif p == 'Walking':
-    #             car_cont += 1
+#     for p in predictions:
+#         if p == 'Still':
+#             still_cont += 1
+#         elif p == 'Car':
+#             walk_cont += 1
+#         elif p == 'Walking':
+#             car_cont += 1
 
-    #     if still_cont >= 2:
-    #         print('You are currently in still position')
-    #     elif walk_cont >= 2:
-    #         print('You are currently walking')
-    #     elif car_cont >= 2:
-    #         print('You are currently in a car')
-    #     predictions.remove(predictions[0])
+#     if still_cont >= 2:
+#         print('You are currently in still position')
+#     elif walk_cont >= 2:
+#         print('You are currently walking')
+#     elif car_cont >= 2:
+#         print('You are currently in a car')
+#     predictions.remove(predictions[0])
