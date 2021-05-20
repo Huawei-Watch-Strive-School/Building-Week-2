aced82c8658597f31ead36bdbd701d62a6ab8f88
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-05-21 02:16:50 +0400

    update

---
+++ time_series.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import random
+import pickle
+from time import time, sleep
+import joblib
+from sklearn.preprocessing import MinMaxScaler,Normalizer,PowerTransformer,QuantileTransformer, RobustScaler,StandardScaler,LabelEncoder, OneHotEncoder,OrdinalEncoder
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline ,make_pipeline
+from sklearn import set_config , impute,compose
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, plot_confusion_matrix,roc_auc_score
+set_config(display='diagram')
+
+
+# In[2]:
+
+
+df = pd.read_csv("./sample.csv")
+X=df.drop('target',axis=1)
+y=df.target
+
+
+# In[7]:
+
+
+preprocessor = joblib.load('preprocessor1.x')
+model = joblib.load('model1.x')
+
+
+# In[8]:
+
+
+X_=preprocessor.transform(X)
+
+
+# In[9]:
+
+
+pred=model.predict(X_)
+print(f"Accuracy { accuracy_score(y, pred)*100}")
+
+
+# In[ ]:
+
+
+predictions = []
+
+for row in range(df.shape[0]):
+    sleep(1)
+    pred = random.randrange(0,3)
+    #pred = model.predict(X_test)
+    if pred == 0:
+        predictions.append(0)
+    elif pred == 1:
+        predictions.append(1)
+    elif pred == 2:
+        predictions.append(2)
+    elif pred == 3:
+        predictions.append(3)
+        
+    if predictions == 5:
+        
+        still_cont  = 0
+        walk_cont  = 0
+        car_cont  = 0
+        bus_cont  = 0
+        
+        for p in predictions:
+            if p == 0:
+                still_cont += 1
+            elif p == 1:
+                walk_cont += 1
+            elif p == 2:
+                car_cont += 1
+            elif p == 3:
+                bus_cont += 1
+        
+        if still_cont >= 2:
+            print('You are currently in still position')
+        elif walk_cont >= 2:
+            print('You are currently walking')
+        elif car_cont >= 2:
+            print('You are currently in a car')
+        elif bus_cont >= 2:
+            print('You are currently in a bu')
+        
+        predictions.remove(predictions[0])
+        
+
