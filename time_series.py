d8c9f26b4b2253b04ea724a0080e724bd6204c09
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-05-21 04:04:42 +0400

    update

--- time_series.py
+++ time_series.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import random
-import pickle
 from time import time, sleep
 import joblib
 from sklearn.preprocessing import MinMaxScaler, Normalizer, PowerTransformer, QuantileTransformer, RobustScaler, StandardScaler, LabelEncoder, OneHotEncoder, OrdinalEncoder
