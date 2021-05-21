c5f8baeffbd6fd57dcbdaa538774059359e76f0e
Nurlan Sarkhanov <nsarkhanov@gmail.com>
2021-05-21 05:33:21 +0400

    update

--- time_series.py
+++ time_series.py
@@ -22,7 +22,7 @@ clf = Pipeline([('pre', preprocessor), ('classification', model)])
 print(df.shape[0])
 predictions = []
 
-for i in range(0, 15):
+for i in range(0, 30):
     x_test = df.drop('target', axis=1).values[i].reshape(1, -1)
     x_test = pd.DataFrame(x_test, columns=df.drop('target', axis=1).columns)
     sleep(1)
@@ -38,7 +38,7 @@ for i in range(0, 15):
         still_cont = 0
         walk_cont = 0
         car_cont = 0
-
+        sleep(1)
         for p in predictions:
             if p == 'Still':
                 still_cont += 1
@@ -46,15 +46,14 @@ for i in range(0, 15):
                 car_cont += 1
             elif p == 'Walking':
                 walk_cont += 1
-            print(predictions)
-            if still_cont >= 3:
-                print('You are currently in still position')
-                continue
-            elif walk_cont >= 2:
-                print('You are currently walking')
-                continue
-            elif car_cont >= 2:
-                print('You are currently in a car')
-
-                continue
-            predictions.remove(predictions[0])
+        print(predictions)
+        if still_cont >= 4:
+            print('You are currently in still position')
+            continue
+        elif walk_cont >= 4:
+            print('You are currently walking')
+            continue
+        elif car_cont >= 4:
+            print('You are currently in a car')
+            continue
+        predictions.remove(predictions[0])
